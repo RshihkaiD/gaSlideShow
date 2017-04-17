2ab0fa1590a9eaf9a9b75c9f1f00e1b3e5ff71ff
RshihkaiD-SourceTree <kevin.chang@tgc-taiwan.com.tw>
2017-04-17 14:07:55 +0800

    1.include basic ga script 2.link icon fonts

---
+++ js/gaSlideShow.js
@@ -0,0 +1,28 @@
+(function() {
+    var sendInit = {
+        id: 'UA-97482190-1',
+        method: 'create'
+    };
+    GASlideShow.init(sendInit)
+    var GASlideShow = new(function() {
+        this.init = function(param) {
+            ga(param.method, param.id, 'auto');
+            ga('send', 'pageview');
+        };
+
+        var includeGAProgram = function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m);
+        }
+
+        includeGAProgram(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    })();
+}());
